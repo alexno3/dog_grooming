950ca3d325f84112b41e23c3b58f3af359909abe
alexno3 <alxgrgsc@gmail.com>
2023-11-21 18:40:19 +0000

    complete index.html, set if page then hover

--- js/script.js
+++ js/script.js
@@ -15,3 +15,44 @@ const secretNumber = generateRandomNumber();
                 document.getElementById('message').innerHTML = 'Oops! Try again.';
             }
         }
+
+// button animation script when clicked
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Get the button element
+    var button = document.getElementById('animatedButton');
+  
+    // Add a click event listener to the button
+    button.addEventListener('click', function() {
+      // Add the 'clicked' class to the button
+      button.classList.add('clicked');
+  
+      // Remove the 'clicked' class after a short delay (adjust the delay as needed)
+      setTimeout(function() {
+        button.classList.remove('clicked');
+      }, 100);
+    });
+  });
+
+  // navbar page link stays hovered when pressed
+
+  document.addEventListener('DOMContentLoaded', function() {
+    // Get all the navbar links
+    var navLinks = document.querySelectorAll('.nav-list a');
+  
+    // Add click event listeners to each link
+    navLinks.forEach(function(link) {
+      link.addEventListener('click', function() {
+        // Remove the 'active' class from all links
+        navLinks.forEach(function(link) {
+          link.classList.remove('active');
+        });
+  
+        // Add the 'active' class to the clicked link
+        link.classList.add('active');
+      });
+    });
+  });
+  
+  
+  
